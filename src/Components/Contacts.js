0b4ddaad0986965541239052d2bbbae62d04a181
Webioann <ioannn7777@gmail.com>
2021-01-19 10:38:05 +0200

    base structure with first component 'Contacts'[10:37]

---
+++ src/Components/Contacts.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Contacts() {
+    return (
+        <div className='wrapper'>
+            <h2>Contacts</h2>
+        </div>
+    )
+}
+
+
+export default Contacts;
